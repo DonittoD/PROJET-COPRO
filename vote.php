2f93cac95f19706051875f73852aaa9c5d5da761
DonittoD <feiteira.daniel@hotmail.com>
2022-12-02 10:03:53 +0100

    addition de commentaire

--- vote.php
+++ vote.php
@@ -4,25 +4,28 @@ session_start();
 require('connexion.php');
 require('_navbar.php');
 
-if (!isset($_SESSION['user_login']) || time() - $_SESSION['login_time'] > 120 )	//check unauthorize user not access in "welcome.php" page
-						{
+// verifier si il y a une session est existante ou si elle a depassé 2Min
+if (!isset($_SESSION['user_login']) || time() - $_SESSION['login_time'] > 120 )
+{
+session_destroy();
 
-							session_destroy();
-
-							header("location: index.php");
-						}
-
+// si non retour a la page de connexion
+header("location: index.php");	
+}
 ?>
 
+<!-- creation du formulaire qui part sur la page valide -->
 <form method="GET" action='valide.php'>
 
+<label for="select_vote">Choisisez la proposition de l'Assemble General:</label>
 
-<label for="select_vote">Choisisez la proposition de l'Assembe General:</label>
+<!-- la barre ou on selecte toutes les propostion de l'assemble -->
 <select name="idProposition" id="select_vote">
-    <option value="">--choisir une option--</option>
+    <option value="">--choisir une option--</option> <!-- la valeur est null par default -->
 
 <?php
 
+// on interoge la base de données pour voir quel sont les proposition qui sont dans l'assemble générale
 foreach ( $db -> query('SELECT libelle, idProposition FROM propositions INNER JOIN propositions_ag ON propositions.id = propositions_ag.idProposition ')as $row) {
     
     echo'<option value="' . $row['idProposition']. '">' . $row['libelle'] . '</option>';
