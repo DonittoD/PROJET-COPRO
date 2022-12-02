a9928cc9876eb5d9f7256482034067402f11f1d5
DonittoD <feiteira.daniel@hotmail.com>
2022-12-02 11:46:15 +0100

    update comments

--- connexion.php
+++ connexion.php
@@ -1,10 +1,12 @@
 <?php   
+    // on donne les paramettrs
     $db_host="localhost";
     $db_user="root";
     $db_password="";
     $db_name="tpcopro";
 
     try{
+        // la connexion a la base de données
         $db = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8",$db_user,$db_password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
