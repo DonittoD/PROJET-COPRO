a10e7d54869858934e3ad77a5856d9b2ad06a199
DonittoD <feiteira.daniel@hotmail.com>
2022-12-02 11:50:25 +0100

    update comments

--- _navbar.php
+++ _navbar.php
@@ -1,3 +1,4 @@
+<!-- la fiche de style css -->
 <style>
         table,td,th{
             border:1px solid black;
@@ -6,16 +7,16 @@
 </style>
 
 <?php
-
+// titre
 echo'<h1>Tableau de bord</h1>';
 
-echo 'welcome '.$_SESSION["name_user"]; 
+// le nom de l'utilisateur
+echo 'Bienvenue '.$_SESSION["name_user"]; 
 
+// les hyperlien pour les autre pages
 echo'<br><a href="dashboard.php">dashboard</a><br>';
-
 echo'<br><a href="listeCopro.php">liste coproprietaires </a><br>';
-
 echo'<a href="listePropo.php">liste propositoin assemble general</a><br>';
-
 echo'<a href="vote.php">insertion de vote</a><br>';
+
 
