a003f15d69ee0b82ca1cf558490dc65e319ab4ed
DonittoD <feiteira.daniel@hotmail.com>
2022-12-14 11:53:04 +0100

    update tableaux

--- resultat.php
+++ resultat.php
@@ -13,7 +13,14 @@ session_destroy();
 header("location: index.php");	
 }
 
-echo'<div class="information_utilisateur">historique des votant <table>';    
+echo'<div class="information_utilisateur">historique de tout les votant <table>';  
+echo'<tr>';
+echo'<td> ID :</td>';
+echo'<td> PRENOM : </td>';
+echo'<td> SUJET : </td>';
+echo'<td> ANNEE : </td>';
+echo'<td> VOTE :</td>';
+echo'</tr>';
     // on interoge la base de données pour verifier si l'utilisateur a voté
     foreach( $db -> query('SELECT * FROM votes INNER JOIN coproprietaires ON coproprietaires.id = votes.idCopro  INNER JOIN propositions ON propositions.id = votes.idProposition ') as $row){ 
         if($row['pourContre']){
@@ -30,6 +37,28 @@ echo'<div class="information_utilisateur">historique des votant <table>';
         echo"<td>". $variable."</td>";
         echo'</tr>';
 
+    }
+    echo'</table>';
+    echo"</div><br>";
+    
+    
+    echo'<div class="information_utilisateur"> tout les copropriétaire :';
+    echo'<table>';
+    echo'<tr>';
+    echo'<td> PRENOM :</td>';
+    echo'<td> NOM : </td>';
+    echo'<td> TANTIEME : </td>';
+    echo'</tr>';
+    
+    // on interoge la base de données pour affiche toute la liste des utilisateur
+    foreach ( $db -> query('SELECT * FROM coproprietaires ORDER BY tantieme ')as $row) {
+        
+        echo '<tr>';
+        echo"<td>". $row['prenom']."</td>";
+        echo"<td>". $row['nom']."</td>";
+        echo"<td>". $row['tantieme']."</td>";
+        echo'</tr>';
+    
     }
     
     echo'</table>';
