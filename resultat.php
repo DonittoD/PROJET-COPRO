c9765618cc8ce2623942f49ddde58cd73429961e
DonittoD <feiteira.daniel@hotmail.com>
2022-12-17 23:43:47 +0100

    update not finish

--- resultat.php
+++ resultat.php
@@ -14,6 +14,16 @@ header("location: index.php");
 }
 
 echo'<div class="information_utilisateur">historique de tout les votant <table>';  
+echo '<select name="idProposition" id="select_vote">
+<option value="">--choisir une option--</option> <!-- la valeur est null par default -->
+';
+foreach ( $db -> query('SELECT libelle, idProposition FROM propositions INNER JOIN propositions_ag ON propositions.id = propositions_ag.idProposition ')as $row) {
+    
+    echo'<option value="' . $row['idProposition']. '">' . $row['libelle'] . '</option>';
+
+}
+echo '</select>';    
+
 echo'<tr>';
 echo'<td> ID :</td>';
 echo'<td> PRENOM : </td>';
@@ -38,6 +48,16 @@ echo'</tr>';
         echo'</tr>';
 
     }
+    foreach($db -> query('SELECT count(*) as total FROM votes INNER JOIN coproprietaires ON coproprietaires.id = votes.idCopro  INNER JOIN propositions ON propositions.id = votes.idProposition')as $row ){
+        echo '<tr>';
+        echo'<td> TOTAL DE VOTANT :</td>';
+        echo'<td></td>';
+        echo'<td></td>';
+        echo'<td></td>';
+        echo"<td>".$row['total']."</td>";
+        echo'</tr>';
+
+    }
     echo'</table>';
     echo"</div><br>";
     
@@ -49,6 +69,8 @@ echo'</tr>';
     echo'<td> NOM : </td>';
     echo'<td> TANTIEME : </td>';
     echo'</tr>';
+
+
     
     // on interoge la base de données pour affiche toute la liste des utilisateur
     foreach ( $db -> query('SELECT * FROM coproprietaires ORDER BY tantieme ')as $row) {
@@ -60,8 +82,18 @@ echo'</tr>';
         echo'</tr>';
     
     }
-    
+    foreach($db -> query('SELECT count(*) as total FROM coproprietaires')as $row ){
+        echo '<tr>';
+        echo'<td> TOTAL UTILISATEUR :</td>';
+        echo'<td></td>';
+        echo"<td>".$row['total']."</td>";
+        echo'</tr>';
+
+    }
+
     echo'</table>';
     echo"</div><br>";
+
+
     
     
